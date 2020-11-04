19faa8ff459fb4ac6839cf2b5d709eb3ec1fa382
catdevdev <catdev.dev@gmail.com>
2020-11-04 20:22:11 +0200

    finish 100%

---
+++ Assets/Scripts/BotController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BotController: MonoBehaviour 
+{    
+    
+    public void Handling() {        
+        print("123");
+    }
+}
--- Assets/Scripts/TriangleController.cs
+++ Assets/Scripts/TriangleController.cs
@@ -2,17 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+
+public class TriangleController : MonoBehaviour
 {
     // Start is called before the first frame update
+    // public GameObject prefabTri;
+    // private UserController userController = new UserController();
     void Start()
-    {
+    { 
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // userController.Processing();
     }
 }
