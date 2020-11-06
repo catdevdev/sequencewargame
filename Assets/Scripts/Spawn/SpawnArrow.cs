c0f283053f56c4d451d57ba699a6d95ebf1b9bce
catdevdev <catdev.dev@gmail.com>
2020-11-06 13:59:30 +0200

    commit for checkout

---
+++ Assets/Scripts/Spawn/SpawnArrow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnArrow : MonoBehaviour
+{
+    [SerializeField]
+    EnemySpawn enemy;
+
+    [SerializeField]
+    UserSpawn user;
+
+    public void Spawn(string type)
+    {
+        switch (type)
+        {
+            case "user":
+                user.Spawn();
+                break;
+            case "enemy":
+                // enemy.Spawn (spawnPointX, spawnPointY);
+                break;
+        }
+    }
+
+    private void Start()
+    {
+        Spawn("user");
+        Spawn("user");
+        Spawn("user");
+   
+    }
+}
---
+++ Assets/Scripts/Spawn/UserSpawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserSpawn : MonoBehaviour
+{
+    public int spawnPointX;
+
+    public int spawnPointY;
+
+    [SerializeField]
+    GameObject userSpaceship;
+
+    public void Spawn()
+    {
+        spawnPointX = Random.Range(-5, 5);
+        spawnPointY = Random.Range(-4, 4);
+        Vector3 spawnPosition = new Vector3(spawnPointX, spawnPointY, 0);
+        Instantiate(userSpaceship, spawnPosition, Quaternion.identity);
+    }
+}
