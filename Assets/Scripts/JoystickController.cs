880b1665d338f8a6613250f75fa819724fe98443
catdevdev <catdev.dev@gmail.com>
2020-11-17 13:39:45 +0200

    fix movement of joystickController

--- Assets/Scripts/JoystickController.cs
+++ Assets/Scripts/JoystickController.cs
@@ -6,24 +6,19 @@ public class JoystickController : TriangleController
 {
     ExecuteJoystickCall executeJoystickCall = new ExecuteJoystickCall();
 
-    private void Start()
-    {
-        executeJoystickCall.joystickInput.posX = 0;
-        executeJoystickCall.joystickInput.posY = 0;
-    }
-
     public void Call(string jsonData)
     {
         executeJoystickCall.Call (jsonData);
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
         // Call("{\"posX\":1,\"posY\":-1}");
         float hozizontal = executeJoystickCall.joystickInput.posX;
         float vertical = executeJoystickCall.joystickInput.posY;
 
         Move (hozizontal, vertical);
+
         if (hozizontal != 0 || vertical != 0)
         {
             Fire();
